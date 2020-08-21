edb79c0740bbaf3163f2c02aac9512187571bd25
Nicci Moyer <niccihendrix@yahoo.com>
2020-08-20 19:09:43 -0500

    post request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,29 @@
+
+let noteList = require("../db.json")
 const { v4: uuidv4 } = require('uuid');
 let fs = require("fs");             
 
-var noteList = require("../db/db.json");
 
 module.exports = function(app) {
 
   app.get("/api/notes", function(req, res) {
     res.json(noteList);
 
-
   });
 
   app.post("/api/notes", function(req, res) {
-      noteList.push(req.body);
+      let note = req.body;
+      note.id = uuidv4(); 
+      noteList.push(note)
       res.json(true);
-    
-    //create unique id on req.body during my psot request
+    fs.writeFile(noteList, (err, data)).push(note)
     });
 
 
-    //is this written correctly?
   app.delete("/api/clear", function(req, res) {
     noteList = [];
     
-    fs.writeFile("./db/db.json", noteList, function (err) {
+    fs.writeFile("./db.json", noteList, function (err) {
         if (err) console.log(error)
     })
     res.json({ ok: true });
