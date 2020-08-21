7f8b42edb1f8f3235a3aa27900a7df2594b4148d
Nicci Moyer <niccihendrix@yahoo.com>
2020-08-21 13:10:02 -0500

    final formatting

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,38 @@
 let noteList = require("../db.json")
 const { v4: uuidv4 } = require('uuid');
-let fs = require("fs");             
+let fs = require("fs");
 
+module.exports = function (app) {
 
-module.exports = function(app) {
-
-  app.get("/api/notes", function(req, res) {
+  app.get("/api/notes", function (req, res) {
     res.json(noteList);
-    fs.readFile("./db.json", noteList, function (err) {
+  });
+
+  app.post("/api/notes", function (req, res) {
+    let note = req.body;
+    note.id = uuidv4();
+    noteList.push(note)
+    res.json(true);
+
+    fs.writeFile("./db.json", JSON.stringify(noteList), function (err) {
       if (err) console.log(error)
-  })
-  });
-
-  app.post("/api/notes", function(req, res) {
-      let note = req.body;
-      note.id = uuidv4(); 
-      noteList.push(note)
-      res.json(true);
-   // fs.writeFile(noteList, (err, data)).push(note)
-    });
-
-    app.delete("/api/notes/:deletedNoteID", function(req, res) {
-      var selectedID = req.params.deletedNoteID;
-      noteList = noteList.filter(note => note.id !== selectedID)
-    
-    fs.writeFile("./db.json", noteList, function (err) {
-        if (err) console.log(error)
+      res.json({ ok: true });
     })
-    res.json({ ok: true });
-
   });
+
+  app.delete("/api/notes/:deletedNoteID", function (req, res) {
+    var selectedID = req.params.deletedNoteID;
+    noteList = noteList.filter(note => note.id !== selectedID)
+
+    fs.writeFile("./db.json", JSON.stringify(noteList), function (err) {
+      if (err) console.log(error)
+      res.json({ ok: true });
+    })
+  });
+
+  app.get("/api/notes/:selectedID", function (req, res) {
+    var selectedID = req.params.selectedID;
+    let [selectedNote] = noteList.filter(note => note.id === selectedID)
+    res.json(selectedNote)
+  })
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,12 @@
 var path = require("path");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/notes", function(req, res) {
+  app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
-  // app.get("/index", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/index.html"));
-  // });
-
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
