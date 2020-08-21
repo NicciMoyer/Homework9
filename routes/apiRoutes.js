c0a4a1ffa2170cc1fd2ec760a0d1c53b43ca3818
Nicci Moyer <niccihendrix@yahoo.com>
2020-08-20 22:08:54 -0500

    delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,3 @@
-
 let noteList = require("../db.json")
 const { v4: uuidv4 } = require('uuid');
 let fs = require("fs");             
@@ -8,7 +7,9 @@ module.exports = function(app) {
 
   app.get("/api/notes", function(req, res) {
     res.json(noteList);
-
+    fs.readFile("./db.json", noteList, function (err) {
+      if (err) console.log(error)
+  })
   });
 
   app.post("/api/notes", function(req, res) {
@@ -22,7 +23,17 @@ module.exports = function(app) {
 
   app.delete("/api/clear", function(req, res) {
     noteList = [];
-    
+
+    //  HOW DO I FINISH THIS FUNCTION... OR DO I USE THE FOR EACH COMMENTED BELOW
+    let deletedNote = noteList.filter(note.id){
+     return note.id 
+    }
+
+    // noteList.forEach(note => {
+    //   if (note.id === deleteNote) {
+        
+    //   }
+    // });
     fs.writeFile("./db.json", noteList, function (err) {
         if (err) console.log(error)
     })
